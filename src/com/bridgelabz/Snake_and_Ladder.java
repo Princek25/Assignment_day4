83905e7b914eb33cdd0f45bc5c6f2721ec272e05
Prince <princek2530@gmail.com>
2022-08-09 18:10:23 +0530

    Welcome Message

---
+++ src/com/bridgelabz/Snake_and_Ladder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+import java.util.Random;
+
+public class Snake_and_Ladder {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game");
+
+
+
+    }
+}
