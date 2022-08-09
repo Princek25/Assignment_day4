7e920172d55c8f51524c11b39e0bc0e2a9aa13fb
Prince <princek2530@gmail.com>
2022-08-09 19:08:03 +0530

    Snake and Ladder

--- src/com/bridgelabz/Snake_and_Ladder.java
+++ src/com/bridgelabz/Snake_and_Ladder.java
@@ -3,14 +3,38 @@ import java.util.Random;
 
 public class Snake_and_Ladder {
     static int position = 0;
+    static int snake = 1;
+    static int ladder = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         System.out.println("Start Position : "+ position);
         int dieNum = dieRoll();
         System.out.println("Die Number : "+ dieNum);
+        action(dieNum);
+        System.out.println("Die Number : "+ position);
 
     }
+    private static void action(int dieNum){
+        int option = (int) (Math.random() * 10) % 3;
+        switch (option){
+            case ladder :
+                System.out.println("It is ladder");
+                position+=dieNum;
+                break;
+            case snake :
+                System.out.println("It is Snake");
+                position+=dieNum;
+                break;
+                if (position<0)
+                    position=0;
+                break;
+            default:
+                System.out.println("No Play");
+        }
+
+    }
+
     private static int dieRoll(){
         Random num = new Random();
         int dieNum = num.nextInt();
